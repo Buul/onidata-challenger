8ac051b15a44a3d90a44638472adbb7ccd63d127
PAULO FIRMINO <paulo.firmino2@gmail.com>
2024-01-22 11:58:24 -0300

    chore(structure files): project structuring and architecture files

---
+++ src/apis/index.ts
@@ -0,0 +1,84 @@
+import axios, {
+  AxiosHeaders,
+  AxiosRequestConfig,
+  RawAxiosRequestHeaders,
+} from 'axios';
+
+import { getToken } from '@/utils/services/auth';
+
+const request = async (params: AxiosRequestConfig) => {
+  const headers: RawAxiosRequestHeaders | AxiosHeaders = {
+    'Cache-Control': 'no-cache',
+    Accept: '*',
+    'Content-Type': 'text/plain; charset=UTF-8',
+  };
+
+  try {
+    const token = getToken();
+
+    if (token) {
+      headers['x-person-token'] = token;
+    }
+  } catch (ex) {
+    if (ex !== 'No current user') {
+      throw ex;
+    }
+  }
+
+  params.headers = headers;
+
+  return axios({
+    ...params,
+  }).then(
+    event => event,
+    error => {
+      throw error;
+    }
+  );
+};
+
+const getBaseUrl = (baseUrl?: string) =>
+  baseUrl || import.meta.env.VITE_APP_WEB_API;
+
+export const get = (props: AxiosRequestConfig, baseURL?: string) =>
+  request({
+    baseURL: getBaseUrl(baseURL),
+    ...props,
+    method: 'GET',
+  });
+
+export const post = (props: AxiosRequestConfig, baseURL?: string) =>
+  request({
+    baseURL: getBaseUrl(baseURL),
+    ...props,
+    method: 'POST',
+  });
+
+export const put = (props: AxiosRequestConfig, baseURL?: string) =>
+  request({
+    baseURL: getBaseUrl(baseURL),
+    ...props,
+    method: 'PUT',
+  });
+
+export const patch = (props: AxiosRequestConfig, baseURL?: string) =>
+  request({
+    baseURL: getBaseUrl(baseURL),
+    ...props,
+    method: 'PATCH',
+  });
+
+export const del = (props: AxiosRequestConfig, baseURL?: string) =>
+  request({
+    baseURL: getBaseUrl(baseURL),
+    ...props,
+    method: 'DELETE',
+  });
+
+export default {
+  del,
+  get,
+  patch,
+  post,
+  put,
+};
