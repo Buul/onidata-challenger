7469647488ece3bf134291a6f8d248aed19f5e7b
PAULO FIRMINO <paulo.firmino2@gmail.com>
2024-01-24 10:11:32 -0300

    feat(routes): added new routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { MainLayout, PrivateRoute } from '@/components';
 import {
   LoginPresentation,
   PageNotFoundPresentation,
+  ProductFormPresentation,
+  ProductsPresentation,
+  ProductViewPresentation,
   RegisterPresentation,
 } from '@/presentation';
 
@@ -29,6 +33,61 @@ const Router: React.FC = () => {
       {renderMultiRoutes({
         paths: ['/', '/sign'],
         element: <LoginPresentation />,
+      })}
+
+      <Route
+        path="/produtos"
+        element={
+          <PrivateRoute>
+            <MainLayout>
+              <ProductsPresentation />
+            </MainLayout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/adicionar-produto"
+        element={
+          <PrivateRoute>
+            <MainLayout>
+              <ProductFormPresentation />
+            </MainLayout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/editar-produto"
+        element={
+          <PrivateRoute>
+            <MainLayout>
+              <ProductFormPresentation />
+            </MainLayout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/visualizar-produto"
+        element={
+          <PrivateRoute>
+            <MainLayout>
+              <ProductViewPresentation />
+            </MainLayout>
+          </PrivateRoute>
+        }
+      />
+
+      {renderMultiRoutes({
+        paths: ['/home', '/configuracoes', '/usuarios'],
+        element: (
+          <PrivateRoute>
+            <MainLayout>
+              <div>Página em construção...</div>
+            </MainLayout>
+          </PrivateRoute>
+        ),
       })}
     </Routes>
   );
